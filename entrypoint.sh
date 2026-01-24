697f510ed8e43a3d1b2eec26b8b7c5c9365ebf8f
gizzy <me@gizzy.pro>
2026-01-24 15:21:20 +0000

    fix: idk hope this fixes issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,5 @@ if [ ! -d "/usr/src/app/cache" ]; then
   mkdir -p "/usr/src/app/cache"
 fi
 
-if [ ! -f "/usr/src/app/cache/.setup" ]; then
-  exec su-exec bun bun run migrate
-  exec su-exec bun touch .setup
-fi
-
 chown -R bun:bun /usr/src/app/cache
 exec su-exec bun bun run src/index.ts
