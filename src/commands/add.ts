3836d1c7a83041b513034742475d522c6dc20d4b
gizzy <nerop455@gmail.com>
2026-03-11 09:02:41 +0000

    feat: wip of add command taking in a param

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -2,6 +2,11 @@ import type { SlackCommandMiddlewareArgs } from "@slack/bolt";
 import type { RequestHandler } from "..";
 import { users } from "../schema/users";
 import { eq } from "drizzle-orm";
+import { projects } from "../schema/projects";
+import checkAPIKey from "../lib/apiKeyCheck";
+import FT from "../lib/ft";
+import { getGenericErrorMessage } from "../lib/genericError";
+type UserRow = typeof users._.inferSelect;
 
 export default {
   name: "add",
@@ -24,6 +29,9 @@ export default {
           response_type: "ephemeral",
         });
 
+      const updateFields: Partial<UserRow> = {};
+
+
       const userData = await pg
         .select()
         .from(users)
@@ -36,41 +44,161 @@ export default {
           response_type: "ephemeral",
         });
 
-      await client.views.open({
-        trigger_id: command.trigger_id,
-        view: {
-          type: "modal",
-          callback_id: callbackId!,
-          title: {
-            type: "plain_text",
-            text: /^[a-z]/i.test(prefix!)
-              ? prefix![0]!.toUpperCase() + prefix!.slice(1)
-              : prefix!,
+      const projectId = command.text.trim()
+      if (!projectId) {
+
+        await client.views.open({
+          trigger_id: command.trigger_id,
+          view: {
+            type: "modal",
+            callback_id: callbackId!,
+            title: {
+              type: "plain_text",
+              text: /^[a-z]/i.test(prefix!)
+                ? prefix![0]!.toUpperCase() + prefix!.slice(1)
+                : prefix!,
+            },
+            private_metadata: JSON.stringify({
+              channel: command.channel_id,
+            }),
+            blocks: [
+              {
+                type: "input",
+                block_id: "projId",
+                label: {
+                  type: "plain_text",
+                  text: "What is the project's id",
+                },
+                element: {
+                  type: "plain_text_input",
+                  action_id: "proj_input",
+                  multiline: false,
+                },
+              },
+            ],
+            submit: {
+              type: "plain_text",
+              text: "Submit",
+            },
           },
-          private_metadata: JSON.stringify({
-            channel: command.channel_id,
-          }),
+        });
+      } else {
+        if (!Number.isInteger(projectId)) return respond({
+          text: `Project ID has to be a integer`,
+          response_type: "ephemeral",
+        })
+        const checkKey = String(userData[0]?.apiKey);
+
+        const working = await checkAPIKey({
+          db: pg,
+          apiKey: checkKey,
+          logger,
+        });
+        if (!working.works)
+          return respond({
+            text: "Flavortown API Key is invalid, provide a valid one.",
+            response_type: "ephemeral",
+          });
+
+        const apiKey = checkKey!;
+
+        const ftClient = new FT(apiKey, logger);
+
+
+        const projectsArr = Array.isArray(userData[0]?.projects)
+          ? Array.from(
+            new Set(
+              userData[0]?.projects.filter(
+                (p): p is number => Number.isInteger(p) && p > 0,
+              ),
+            ),
+          )
+          : [];
+
+        if (projectsArr.includes(Number(projectId))) {
+          return respond({
+            text: "Project already registered",
+            response_type: "ephemeral",
+
+          });
+        }
+
+        projectsArr.push(Number(projectId));
+        if (!userData[0]?.userId) {
+          updateFields.userId = command.user_id;
+        }
+
+        if (!userData[0]?.channel) {
+          updateFields.channel = command.channel_id;
+        }
+
+        updateFields.projects = projectsArr;
+        await pg.update(users).set(updateFields).where(eq(users.userId, command.user_id));
+
+        const freshProject = await ftClient.project({ id: Number(projectId) });
+        if (!freshProject || !freshProject.status) {
+          return respond({
+            text: "Unexpected error has occurred.",
+            response_type: "ephemeral"
+          });
+        }
+
+        if (!freshProject.ok || !Object.keys(freshProject.data)?.length) {
+          switch (freshProject.status) {
+            case 404:
+              return respond({
+                text: "Project doesn't exist.",
+                response_type: "ephemeral"
+                ,
+              });
+            default:
+              const msg = getGenericErrorMessage(freshProject.status, prefix!);
+              return respond({
+                text: msg ?? "Unexpected error has occured!",
+                response_type: "ephemeral"
+              });
+          }
+        }
+
+        const devlogIds = Array.isArray(freshProject.data.devlog_ids)
+          ? freshProject.data.devlog_ids
+          : [];
+
+        await pg
+          .insert(projects)
+          .values({
+            id: Number(projectId),
+            devlogIds,
+          })
+          .onConflictDoUpdate({
+            target: projects.id,
+            set: {
+              devlogIds,
+            },
+          });
+
+        return respond({
+          unfurl_links: false,
+          unfurl_media: false,
           blocks: [
             {
-              type: "input",
-              block_id: "projId",
-              label: {
-                type: "plain_text",
-                text: "What is the project's id",
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `:woah-dino: <https://flavortown.hackclub.com/projects/${Number(projectId)}|${freshProject.data.title}'s> devlogs just got subscribed to the channel. :yay:`,
               },
-              element: {
-                type: "plain_text_input",
-                action_id: "proj_input",
-                multiline: false,
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `> ${freshProject.data.description}`,
               },
             },
           ],
-          submit: {
-            type: "plain_text",
-            text: "Submit",
-          },
-        },
-      });
+          response_type: "in_channel"
+        });
+      }
     } catch (error: any) {
       if (
         error.code === "slack_webapi_platform_error" &&
