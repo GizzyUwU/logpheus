f6512219ca83ed366d2f33ec6fa0acee7725e2b8
gizzy <nerop455@gmail.com>
2026-04-14 07:14:27 +0100

    feat: move command to move devlog posts to diff channels

---
+++ src/commands/move.ts
@@ -0,0 +1,118 @@
+import type { SlackCommandMiddlewareArgs } from "@slack/bolt";
+import type { RequestHandler } from "..";
+import { users } from "../schema/users";
+import { eq } from "drizzle-orm";
+import { projects } from "../schema/projects";
+import checkAPIKey from "../lib/apiKeyCheck";
+import FT from "../lib/ft";
+import { getGenericErrorMessage } from "../lib/genericError";
+type UserRow = typeof users._.inferSelect;
+
+export default {
+  name: "move",
+  params: "[channelId]",
+  desc: "Move the devlog posts to a different channel!",
+  execute: async (
+    { command, respond }: SlackCommandMiddlewareArgs,
+    { logger, client, pg, prefix }: RequestHandler,
+  ) => {
+    try {
+      const channelId = command.text.trim();
+      const channel = await client.conversations.info({
+        channel: command.channel_id,
+      });
+      const newChannel = await client.conversations.info({
+        channel: channelId,
+      });
+      if (!channel)
+        return await respond({
+          text: "If you are running this in a private channel then you have to add bot manually first to the channel. CHANNEL_NOT_FOUND",
+          response_type: "ephemeral",
+        });
+      if (command.user_id !== channel.channel?.creator)
+        return await respond({
+          text: "You can only run this command in a channel that you are the creator of",
+          response_type: "ephemeral",
+        });
+      if (command.user_id !== newChannel.channel?.creator)
+        return await respond({
+          text: "You can only run this command to move to channel you own",
+          response_type: "ephemeral",
+        });
+
+      const updateFields: Partial<UserRow> = {};
+      const userData = await pg
+        .select()
+        .from(users)
+        .limit(1)
+        .where(eq(users.userId, command.user_id));
+
+      if (userData.length === 0)
+        return await respond({
+          text: `Run /${prefix}-register first to be able to run this command.`,
+          response_type: "ephemeral",
+        });
+
+      const checkKey = String(userData[0]?.apiKey);
+
+      const working = await checkAPIKey({
+        db: pg,
+        apiKey: checkKey,
+        logger,
+      });
+      if (!working.works)
+        return respond({
+          text: "Flavortown API Key is invalid, provide a valid one.",
+          response_type: "ephemeral",
+        });
+
+      if (!userData[0]?.userId) {
+        updateFields.userId = command.user_id;
+      }
+
+      updateFields.channel = channelId;
+
+      await pg
+        .update(users)
+        .set(updateFields)
+        .where(eq(users.userId, command.user_id));
+
+      await respond({
+        response_type: "ephemeral",
+        text: "Moved successfully",
+      });
+
+      return client.chat.postMessage({
+        unfurl_links: false,
+        unfurl_media: false,
+        channel: channelId,
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `:woah-dino: Devlog Posts moved to here! :yay:`,
+            },
+          },
+        ],
+      });
+    } catch (error: any) {
+      if (
+        error.code === "slack_webapi_platform_error" &&
+        error.data?.error === "channel_not_found"
+      ) {
+        await respond({
+          text: "If you are running this in a private channel then you have to add bot manually first to the channel. CHANNEL_NOT_FOUND",
+          response_type: "ephemeral",
+        });
+        return;
+      } else {
+        logger.error({ error });
+        await respond({
+          text: "An unexpected error occurred!",
+          response_type: "ephemeral",
+        });
+      }
+    }
+  },
+};
