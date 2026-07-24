d6e018f2d408b92434468f2dce54f640514aff4a
gizzy <nerop455@gmail.com>
2026-07-24 14:04:46 +0100

    fix: move env to export to try fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ if [ ! -d "/usr/src/app/cache" ]; then
 fi
 
 chown -R bun:bun /usr/src/app/cache
-exec su-exec bun GIT_COMMIT_SHA="$(git --git-dir=/usr/src/app/.git rev-parse HEAD 2>/dev/null || echo unknown)" bun run src/index.ts
+export GIT_COMMIT_SHA="$(git --git-dir=/usr/src/app/.git rev-parse HEAD 2>/dev/null || echo unknown)"
+exec su-exec bun bun run src/index.ts
